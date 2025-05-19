afcf6a86a68347b304f841379c44752870e2f973
Yator1 <titusyatz17@gmail.com>
2025-05-19 16:08:30 +0300

    changes of api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = window.location.hostname === "localhost" ? "http://localhost/shehena.co.ke/www/endpoints" : "https://admin.shehena.co.ke/endpoints";
 // API Endpoints
 const ENDPOINTS = {
   AUTH: {
